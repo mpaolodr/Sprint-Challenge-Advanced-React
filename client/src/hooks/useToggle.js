80d1462276d06cfde65db83a37c246d746db787f
mpaolodr <mpaolodr_1504@yahoo.com>
2020-01-17 11:49:41 -0500

    added useToggle custom hook

---
+++ client/src/hooks/useToggle.js
@@ -0,0 +1,22 @@
+import { useEffect } from "react";
+import { useLocalStorage } from "./useLocalStorage";
+
+const useToggle = (key, target) => {
+  const [value, setValue] = useLocalStorage(key, false);
+
+  useEffect(() => {
+    const container = document.querySelector(`.${target}`);
+    if (value) {
+      container.classList.add("toggled");
+    } else {
+      container.classList.remove("toggled");
+    }
+  }, [value]);
+
+  const setter = e => {
+    e.preventDefault();
+    setValue(!value);
+  };
+
+  return [value, setValue];
+};
